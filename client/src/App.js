4813a7e0bfb443a5ab937483f529996b17b48411
aashutoshrathi <aashutoshrathi@gmail.com>
2019-01-23 20:01:07 +0530

    Basic setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
+import ReactLoading from "react-loading";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
+import { Button } from "antd";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = {
+    storageValue: 0,
+    web3: null,
+    accounts: null,
+    contract: null,
+    loading: false
+  };
 
   componentDidMount = async () => {
     try {
@@ -20,7 +28,7 @@ class App extends Component {
       const deployedNetwork = SimpleStorageContract.networks[networkId];
       const instance = new web3.eth.Contract(
         SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+        deployedNetwork && deployedNetwork.address
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
@@ -29,42 +37,45 @@ class App extends Component {
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        `Failed to load web3, accounts, or contract. Check console for details.`
       );
       console.error(error);
     }
   };
 
-  runExample = async () => {
+  handleClick = async event => {
+    this.setState({ loading: true });
     const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
+    var val = 7;
+    await contract.methods.set(val).send({ from: accounts[0] });
+    const res = await contract.methods.get().call();
+    this.setState({ storageValue: res, loading: false });
   };
 
   render() {
     if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+      return (
+        <div className="App center">
+          <ReactLoading
+            type={"balls"}
+            color={"blue"}
+            height={"10%"}
+            width={"10%"}
+          />
+        </div>
+      );
     }
     return (
       <div className="App">
         <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
         <div>The stored value is: {this.state.storageValue}</div>
+        <Button
+          type="primary"
+          loading={this.state.loading}
+          onClick={this.handleClick.bind(this)}
+        >
+          Set Storage
+        </Button>
       </div>
     );
   }
--- truffle-config.js
+++ truffle-config.js
@@ -3,5 +3,12 @@ const path = require("path");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks:{
+    development:{
+      host: "127.0.0.1",
+      port: "8545",
+      network_id: "*",
+    }
+  },
   contracts_build_directory: path.join(__dirname, "client/src/contracts")
 };
