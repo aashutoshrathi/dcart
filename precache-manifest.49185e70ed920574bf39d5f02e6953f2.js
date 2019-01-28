0a9ab5df5e96c6b9a6fc65a6db5db882880ab86a
aashutoshrathi <aashutoshrathi@gmail.com>
2019-01-28 12:27:30 +0530

    Updates

---
+++ precache-manifest.49185e70ed920574bf39d5f02e6953f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4eb5b11edd5cae398cbac0ea9b8f140",
+    "url": "/final-project-aashutoshrathi/static/media/logo.f4eb5b11.png"
+  },
+  {
+    "revision": "a441cd0d33473444aafb",
+    "url": "/final-project-aashutoshrathi/static/js/runtime~main.a441cd0d.js"
+  },
+  {
+    "revision": "d70ce1443178a1a6ef4d",
+    "url": "/final-project-aashutoshrathi/static/js/main.d70ce144.chunk.js"
+  },
+  {
+    "revision": "3e3ae8911d19a323dce8",
+    "url": "/final-project-aashutoshrathi/static/js/1.3e3ae891.chunk.js"
+  },
+  {
+    "revision": "d70ce1443178a1a6ef4d",
+    "url": "/final-project-aashutoshrathi/static/css/main.d0ad97dd.chunk.css"
+  },
+  {
+    "revision": "3e3ae8911d19a323dce8",
+    "url": "/final-project-aashutoshrathi/static/css/1.616b2fd0.chunk.css"
+  },
+  {
+    "revision": "c5ec91720978019ac85f04d491c3515d",
+    "url": "/final-project-aashutoshrathi/index.html"
+  }
+];
